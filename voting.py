564c5ead8883041a2cb20eaf36fdf23383d4c002
Ishan Mandrekar <imandrekar@extron.com>
2018-04-20 18:22:51 -0400

    Consolidate and cleanup Poll and Player API

--- voting.py
+++ voting.py
@@ -10,6 +10,9 @@ class Vote(object):
         self._result = result if (type(result) == bool) else False
         self._timestamp = datetime.now()
 
+    def __repr__(self):
+        return "<Vote: {}, {}, {}>".format(self._poll, self._result, self._timestamp)
+
     @property
     def poll(self):
         return self._poll
@@ -28,6 +31,9 @@ class Vote(object):
 class Selection(Vote):
     def __init__(self, poll, result):
         super(Selection, self).__init__(poll, result)
+
+    def __repr__(self):
+        return "<Selection: {}, {}, {}>".format(self._poll, self._result, self._timestamp)
 
 
 class Player(object):
@@ -66,59 +72,42 @@ class Player(object):
                 return selection
         return None
 
-    def add_game_vote(self, poll, result=True):
-        if self.active:
-            vote = self._find_vote(self.game_votes, poll)
+    def add_vote(self, poll, result=True):
+        ret = False
+        if type(poll) == Poll:
+            if not self.active or (poll.type == "water" and not self.water_active) or (poll.type == "umpiring" and not self.umpiring_active):
+                return ret
+            votes = getattr(self, "{}_votes".format(poll.type), None)
+            if votes is None:
+                return ret
+            ret = True
+            vote = self._find_vote(votes, poll)
             if vote:
                 vote.result = result
             else:
-                self.game_votes.append(Vote(poll, result))
+                votes.append(Vote(poll, result))
+        return ret
 
-    def add_water_vote(self, poll, result=True):
-        if self.active and self.water_active:
-            vote = self._find_vote(self.water_votes, poll)
-            if vote:
-                vote.result = result
-            else:
-                self.water_votes.append(Vote(poll, result))
-
-    def add_umpiring_vote(self, poll, result=True):
-        if self.active and self.umpiring_active:
-            vote = self._find_vote(self.umpiring_votes, poll)
-            if vote:
-                vote.result = result
-            else:
-                self.umpiring_votes.append(Vote(poll, result))
-
-    def add_game_selection(self, poll, result):
-        if self.active:
-            vote = self._find_vote(self.game_votes, poll)
+    def add_selection(self, poll, result=True):
+        ret = False
+        if type(poll) == Poll:
+            if not self.active or (poll.type == "water" and not self.water_active) or (poll.type == "umpiring" and not self.umpiring_active):
+                return ret
+            votes = getattr(self, "{}_votes".format(poll.type), None)
+            if votes is None:
+                return ret
+            vote = self._find_vote(votes, poll)
             if vote and vote.result:
-                selection = self._find_selection(self.game_selections, poll)
+                selections = getattr(self, "{}_selections".format(poll.type), None)
+                if selections is None:
+                    return ret
+                ret = True
+                selection = self._find_selection(selections, poll)
                 if selection:
                     selection.result = result
                 else:
-                    self.game_selections.append(Selection(poll, result))
-
-    def add_water_selection(self, poll, result):
-        if self.active and self.water_active:
-            vote = self._find_vote(self.water_votes, poll)
-            if vote and vote.result:
-                selection = self._find_selection(self.water_selections, poll)
-                if selection:
-                    selection.result = result
-                else:
-                    self.water_selections.append(Selection(poll, result))
-
-    def add_umpiring_selection(self, poll, result):
-        if self.active and self.umpiring_active:
-            vote = self._find_vote(self.umpiring_votes, poll)
-            if vote and vote.result:
-                selection = self._find_selection(self.umpiring_selections, poll)
-                if selection:
-                    selection.result = result
-                else:
-                    self.umpiring_selections.append(Selection(poll, result))
+                    selections.append(Selection(poll, result))
+        return ret
 
     @property
     def num_game_selections(self):
@@ -175,6 +164,9 @@ class Poll(object):
             for player in self._roster.get_umpiring_active_players():
                 self.players[player.id] = player
 
+    def __repr__(self):
+        return "<Poll: {}, {}, {}>".format(self.id, self.type, self.desc)
+
     def _is_voting_open(self):
         if self.open_timestamp and not self.close_timestamp and not self.finalize_timestamp:
             return True
@@ -184,17 +176,6 @@ class Poll(object):
         if self.open_timestamp and self.close_timestamp and not self.finalize_timestamp:
             return True
         return False
-
-    def _get_voted_players(self):
-        voted_players = []
-        for player in self.players.values():
-            votes = getattr(player, "{}_votes".format(self.type), [])
-            for vote in votes:
-                if (vote.poll == self):
-                    if vote.result:
-                        voted_players.append(player)
-                    break
-        return voted_players
 
     def open(self):
         """
@@ -219,48 +200,34 @@ class Poll(object):
 
     def log_vote(self, player_id, result=True):
         """
-        attempt log a single vote
-        :param: player_id: int
+        attempt to log a single vote
+        :param: player_id: str
         :param: result: bool
         return: whether voting was successful
         rtype: bool
         """
         if not self._is_voting_open():
             return False
-        ret = True
+        ret = False
         if player_id in self.players.keys():
             player = self.players[player_id]
-            if self.type == "game":
-                player.add_game_vote(self, result)
-            elif self.type == "water":
-                player.add_water_vote(self, result)
-            elif self.type == "umpiring":
-                player.add_umpiring_vote(self, result)
-            else:
-                ret = False
+            ret = player.add_vote(self, result)
         return ret
 
     def log_selection(self, player_id, result=True):
         """
-        attempt log a single selection
-        :param: player_id: int
+        attempt to log a single selection
+        :param: player_id: str
         :param: result: bool
         return: whether selection was successful
         rtype: bool
         """
         if not self._is_selection_open():
             return False
-        ret = True
+        ret = False
         if player_id in self.players.keys():
             player = self.players[player_id]
-            if self.type == "game":
-                player.add_game_selection(self, result)
-            elif self.type == "water":
-                player.add_water_selection(self, result)
-            elif self.type == "umpiring":
-                player.add_umpiring_selection(self, result)
-            else:
-                ret = False
+            ret = player.add_selection(self, result)
         return ret
 
     def auto_vote(self):
@@ -273,8 +240,9 @@ class Poll(object):
         if not self._is_voting_open():
             return voted
         for player_id, player in self.players.items():
-            self.log_vote(player_id)
-            voted.append(player)
+            ret = self.log_vote(player_id)
+            if ret:
+                voted.append(player)
         return voted
 
     def auto_select(self):
@@ -287,36 +255,68 @@ class Poll(object):
         if not self._is_selection_open():
             return selected
         if self.type == "game":
-            players = self._get_voted_players()
+            players = self.get_voted_players()
             # sort by previously recorded num_game_selections in ascending order
             players.sort(key=attrgetter("num_game_selections"))
             # select upto 11 players
             for player in players[:11]:
-                self.log_selection(player.id)
-                selected.append(player)
+                ret = self.log_selection(player.id)
+                if ret:
+                    selected.append(player)
         elif self.type == "water":
-            players = self._get_voted_players()
+            players = self.get_voted_players()
             # sort by previously recorded num_water_selections in ascending order
             players.sort(key=attrgetter("num_water_selections"))
             # get the first player
             player = players[0] if len(players) else None
             if player:
-                self.log_selection(player.id)
-                selected.append(player)
+                ret = self.log_selection(player.id)
+                if ret:
+                    selected.append(player)
         elif self.type == "umpiring":
-            players = self._get_voted_players()
+            players = self.get_voted_players()
             # sort by previously recorded num_umpiring_selections in ascending order
             players.sort(key=attrgetter("num_umpiring_selections"))
             if len(players) >= 2:
                 # get the first 2 players
                 player1, player2 = players[0], players[1]
-                self.log_selection(player1.id)
-                selected.append(player1)
-                self.log_selection(player2.id)
-                selected.append(player2)
-        if selected:
-            self.finalize()
+                ret = self.log_selection(player1.id)
+                if ret:
+                    selected.append(player1)
+                ret = self.log_selection(player2.id)
+                if ret:
+                    selected.append(player2)
         return selected
+
+    def get_voted_players(self):
+        """
+        return: voted players
+        rtype: list of Player objects
+        """
+        voted_players = []
+        for player in self.players.values():
+            votes = getattr(player, "{}_votes".format(self.type), [])
+            for vote in votes:
+                if (vote.poll == self):
+                    if vote.result:
+                        voted_players.append(player)
+                    break
+        return voted_players
+
+    def get_selected_players(self):
+        """
+        return: selected players
+        rtype: list of Player objects
+        """
+        selected_players = []
+        for player in self.players.values():
+            selections = getattr(player, "{}_selections".format(self.type), [])
+            for selection in selections:
+                if (selection.poll == self):
+                    if selection.result:
+                        selected_players.append(player)
+                    break
+        return selected_players
 
 
 if __name__ == "__main__":
@@ -329,10 +329,10 @@ if __name__ == "__main__":
     # water
     poll = Poll(type="water", desc="Test Water Poll 1", open=True)
     voted = poll.auto_vote()
-    # print("water voted: {}".format(voted))
     poll.close()
     selected = poll.auto_select()
     print("water selection: {}".format(selected))
+    poll.finalize()
 
     # umpiring
     poll = Poll(type="umpiring", desc="Test Umpiring Poll 1", open=True)
@@ -340,26 +340,37 @@ if __name__ == "__main__":
     poll.close()
     selected = poll.auto_select()
     print("umpiring selection: {}".format(selected))
+    poll.finalize()
     # umpiring
     poll = Poll(type="umpiring", desc="Test Umpiring Poll 2", open=True)
     poll.auto_vote()
     poll.close()
     selected = poll.auto_select()
     print("umpiring selection: {}".format(selected))
+    poll.finalize()
 
     # game
     poll = Poll(type="game", desc="Test Game Poll 1", open=True, roster=roster)
     # first 14 vote yes
-    for player in players[:14]:
-        player.add_game_vote(poll)
+    players = players[:14]
+    for player in players:
+        poll.log_vote(player.id)
     poll.close()
     selected = poll.auto_select()
     print("game selection: {}".format(selected))
+    poll.finalize()
     # game
     poll = Poll(type="game", desc="Test Game Poll 2", open=True, roster=roster)
     # last 14 vote yes
-    for player in players[-14:]:
-        player.add_game_vote(poll)
+    players = players[-14:]
+    for player in players:
+        poll.log_vote(player.id)
     poll.close()
     selected = poll.auto_select()
-    print("game selection: {}".format(selected))
+    print("auto game selection: {}".format(selected))
+    # tweak the auto selection
+    unselected = list(set(players) - set(selected))
+    poll.log_selection(selected[0].id, False)
+    poll.log_selection(unselected[0].id, True)
+    print("tweaked game selection: {}".format(poll.get_selected_players()))
+    poll.finalize()
